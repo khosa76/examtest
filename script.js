cf9ed62175d6c2b97ad25194026d2c2d7bfc6d7d
Jaskaran Singh Khosa <84892564+khosa76@users.noreply.github.com>
2025-11-22 18:19:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,471 @@ const validPayload = {
     "data": {
         "learnerTestAttemptId": "e1288d71-1ecb-43c2-b4d1-e724065fa013",
         "timeLeft": 2700,
+        "currentSectionIndex": 0,
+        "currentQuestionIndex": 0,
+        "answers": {},
+        "flaggedQuestions": [],
+        "isAutoNextEnabled": false,
+        "testId": "422d460d-7a82-4416-b0f4-967c10a3fe9e",
         "title": "MOCK TEST",
+        "duration": 2700,
         "sections": [
             {
                 "id": "57d82046-ebf1-4d65-8735-7d1d3e2b9814",
                 "title": "Section 1",
-                "questions": []
+                "sequence": 1,
+                "questions": [
+                    {
+                        "id": "fa916117-041f-410f-b55d-07a7ff90162e",
+                        "sequence": 1,
+                        "type": "SINGLE_CHOICE",
+                        "text": "My friend __ to temple daily.",
+                        "marks": 1,
+                        "options": [
+                            {
+                                "id": 1,
+                                "text": "Go",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 2,
+                                "text": "Goes",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 3,
+                                "text": "Went",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 4,
+                                "text": "Gone",
+                                "imageUrls": []
+                            }
+                        ],
+                        "imageUrls": []
+                    },
+                    {
+                        "id": "95a39a01-719d-4902-9c73-b88b76529f0e",
+                        "sequence": 2,
+                        "type": "SINGLE_CHOICE",
+                        "text": "Bread and butter ,__. A wholesome food.",
+                        "marks": 1,
+                        "options": [
+                            {
+                                "id": 1,
+                                "text": "Is",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 2,
+                                "text": "Are",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 3,
+                                "text": "Am",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 4,
+                                "text": "Had",
+                                "imageUrls": []
+                            }
+                        ],
+                        "imageUrls": []
+                    },
+                    {
+                        "id": "8c9a9361-c4cd-4630-bedd-cc9b37cbd6f9",
+                        "sequence": 3,
+                        "type": "SINGLE_CHOICE",
+                        "text": "His new novel is due to appear ___",
+                        "marks": 1,
+                        "options": [
+                            {
+                                "id": 1,
+                                "text": "short",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 2,
+                                "text": "Shortened",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 3,
+                                "text": "shortly",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 4,
+                                "text": "shorter",
+                                "imageUrls": []
+                            }
+                        ],
+                        "imageUrls": []
+                    },
+                    {
+                        "id": "649c97a1-f2e0-4cd5-acfb-385963755e3d",
+                        "sequence": 4,
+                        "type": "SINGLE_CHOICE",
+                        "text": "A large crowd ___ expected at the match.",
+                        "marks": 1,
+                        "options": [
+                            {
+                                "id": 1,
+                                "text": "Is",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 2,
+                                "text": "Am",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 3,
+                                "text": "Are",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 4,
+                                "text": "Were",
+                                "imageUrls": []
+                            }
+                        ],
+                        "imageUrls": []
+                    },
+                    {
+                        "id": "71f76728-f29c-40a8-99bb-ed93808488dc",
+                        "sequence": 5,
+                        "type": "SINGLE_CHOICE",
+                        "text": "John and Mary __ gone for a holiday.",
+                        "marks": 1,
+                        "options": [
+                            {
+                                "id": 1,
+                                "text": "Has",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 2,
+                                "text": "Have",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 3,
+                                "text": "Are",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 4,
+                                "text": "Am",
+                                "imageUrls": []
+                            }
+                        ],
+                        "imageUrls": []
+                    },
+                    {
+                        "id": "b955bbf8-e784-4510-b983-a5f130f585ac",
+                        "sequence": 6,
+                        "type": "SINGLE_CHOICE",
+                        "text": "Ten pounds __. A lot of money.",
+                        "marks": 1,
+                        "options": [
+                            {
+                                "id": 1,
+                                "text": "Is",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 2,
+                                "text": "Am",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 3,
+                                "text": "Are",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 4,
+                                "text": "Were",
+                                "imageUrls": []
+                            }
+                        ],
+                        "imageUrls": []
+                    },
+                    {
+                        "id": "b3bb73a9-6be0-42a7-bb33-2c76cef91cb3",
+                        "sequence": 7,
+                        "type": "SINGLE_CHOICE",
+                        "text": "He _.. late",
+                        "marks": 1,
+                        "options": [
+                            {
+                                "id": 1,
+                                "text": "Slept",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 2,
+                                "text": "Slapped",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 3,
+                                "text": "Slappet",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 4,
+                                "text": "Slap",
+                                "imageUrls": []
+                            }
+                        ],
+                        "imageUrls": []
+                    },
+                    {
+                        "id": "98c59f56-305f-487f-8d10-4dba939c2b2a",
+                        "sequence": 8,
+                        "type": "SINGLE_CHOICE",
+                        "text": "These shoes __.. almost worn out.",
+                        "marks": 1,
+                        "options": [
+                            {
+                                "id": 1,
+                                "text": "Are",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 2,
+                                "text": "Is",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 3,
+                                "text": "Am",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 4,
+                                "text": "Has",
+                                "imageUrls": []
+                            }
+                        ],
+                        "imageUrls": []
+                    },
+                    {
+                        "id": "0a3fa5af-2c7c-4b80-a781-45b15d1922fe",
+                        "sequence": 9,
+                        "type": "SINGLE_CHOICE",
+                        "text": "He went to a ___",
+                        "marks": 1,
+                        "options": [
+                            {
+                                "id": 1,
+                                "text": "Fare",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 2,
+                                "text": "Fair",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 3,
+                                "text": "Faired",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 4,
+                                "text": "Fared",
+                                "imageUrls": []
+                            }
+                        ],
+                        "imageUrls": []
+                    },
+                    {
+                        "id": "39589a71-f495-4d7d-97d0-2088d241de91",
+                        "sequence": 10,
+                        "type": "SINGLE_CHOICE",
+                        "text": "This kind of tree grows very ___..",
+                        "marks": 1,
+                        "options": [
+                            {
+                                "id": 1,
+                                "text": "Slow",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 2,
+                                "text": "Slowly",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 3,
+                                "text": "Slower",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 4,
+                                "text": "Slowest",
+                                "imageUrls": []
+                            }
+                        ],
+                        "imageUrls": []
+                    },
+                    {
+                        "id": "b95f5f3f-c7e3-4f16-9310-2026b28821f6",
+                        "sequence": 11,
+                        "type": "SINGLE_CHOICE",
+                        "text": "What __ latest news?",
+                        "marks": 1,
+                        "options": [
+                            {
+                                "id": 1,
+                                "text": "Is",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 2,
+                                "text": "Am",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 3,
+                                "text": "Are",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 4,
+                                "text": "Have",
+                                "imageUrls": []
+                            }
+                        ],
+                        "imageUrls": []
+                    },
+                    {
+                        "id": "120e29dc-45f1-4ef7-9aa6-941e58a57b6e",
+                        "sequence": 12,
+                        "type": "SINGLE_CHOICE",
+                        "text": "All the food __. Been eaten .",
+                        "marks": 1,
+                        "options": [
+                            {
+                                "id": 1,
+                                "text": "Has",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 2,
+                                "text": "Have",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 3,
+                                "text": "Had",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 4,
+                                "text": "Are",
+                                "imageUrls": []
+                            }
+                        ],
+                        "imageUrls": []
+                    },
+                    {
+                        "id": "2f4a6341-6d59-454c-a5c1-019933bcb2bd",
+                        "sequence": 13,
+                        "type": "SINGLE_CHOICE",
+                        "text": "A cow __ grass",
+                        "marks": 1,
+                        "options": [
+                            {
+                                "id": 1,
+                                "text": "Eats",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 2,
+                                "text": "Were eaten",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 3,
+                                "text": "Eaten",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 4,
+                                "text": "Eat",
+                                "imageUrls": []
+                            }
+                        ],
+                        "imageUrls": []
+                    },
+                    {
+                        "id": "ab69548a-1e01-40ec-a405-dcfd7e337813",
+                        "sequence": 14,
+                        "type": "SINGLE_CHOICE",
+                        "text": "The surface of this table feels __..",
+                        "marks": 1,
+                        "options": [
+                            {
+                                "id": 1,
+                                "text": "Roughened",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 2,
+                                "text": "Rough",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 3,
+                                "text": "Roughly",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 4,
+                                "text": "Roughed",
+                                "imageUrls": []
+                            }
+                        ],
+                        "imageUrls": []
+                    },
+                    {
+                        "id": "70696e44-cf8e-47a2-8d12-9a0826fc7377",
+                        "sequence": 15,
+                        "type": "SINGLE_CHOICE",
+                        "text": "That barrel sounds __.",
+                        "marks": 1,
+                        "options": [
+                            {
+                                "id": 1,
+                                "text": "Hollow",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 2,
+                                "text": "Hollowly",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 3,
+                                "text": "Hollowed",
+                                "imageUrls": []
+                            },
+                            {
+                                "id": 4,
+                                "text": "Hollows",
+                                "imageUrls": []
+                            }
+                        ],
+                        "imageUrls": []
+                    }
+                ]
             }
         ]
     }
